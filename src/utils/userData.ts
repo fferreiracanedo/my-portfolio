a7cbb2e5c9a6ac4fe4eec0c1c0e26884e7a9ac87
Mateus Ferreira <mateusferreiracanedo@gmail.com>
2022-09-28 13:29:00 -0300

    final test

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Mateus Ferreira",
+  githubUser: "fferreiracanedo",
+  linkedinUser: "fferreiracanedo",
+  whatsappNumber: "+55519814400541",
+  emailUser: "mateusferreiracanedo@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
